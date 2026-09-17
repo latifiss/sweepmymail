b3436e174a491566d3a2a6f97acf2a4d18942e54
Latif Issaka <83374530+latifiss@users.noreply.github.com>
2026-09-17 13:44:46 +0000

    Make inbox messages openable for full reading and replies

--- frontend/components/mailLine.tsx
+++ frontend/components/mailLine.tsx
@@ -1,19 +1,23 @@
 'use client'
 
-import React from 'react';
+import Link from 'next/link'
+import React from 'react'
 
 type MailLineProps = {
   date: string
   content: string
+  messageId?: string
 }
 
-const MailLine = ({ date, content }: MailLineProps) => {
-  return (
-      <div className='mail'>
-          <p className='mail__date'>{date}</p>
-          <p className='mail__content'>{content}</p>
+const MailLine = ({ date, content, messageId }: MailLineProps) => {
+  const contentBlock = (
+    <div className='mail'>
+      <p className='mail__date'>{date}</p>
+      <p className='mail__content'>{content}</p>
     </div>
   )
+
+  return messageId ? <Link href={`/compose?messageId=${encodeURIComponent(messageId)}&mode=reply`} style={{ textDecoration: 'none' }}>{contentBlock}</Link> : contentBlock
 }
 
 export default MailLine
