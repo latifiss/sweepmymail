e58f79cb74a89c6d0e635cc4507ff9ee2068acfd
Latif Issaka <83374530+latifiss@users.noreply.github.com>
2026-09-17 13:45:04 +0000

    Add compose navigation to desktop inbox tabs

--- frontend/components/tabview.tsx
+++ frontend/components/tabview.tsx
@@ -13,6 +13,7 @@ type Tab = {
 
 const tabs: Tab[] = [
   { id: "emails", label: "Emails", icon: "/icons/emails.svg", href: "/emails" },
+  { id: "compose", label: "Compose", icon: "/icons/emails.svg", href: "/compose" },
   { id: "subscriptions", label: "Subscriptions", icon: "/icons/subscriptions.svg", href: "/subscriptions" },
   { id: "info", label: "Info", icon: "/icons/info.svg", href: "/info" },
 ];
